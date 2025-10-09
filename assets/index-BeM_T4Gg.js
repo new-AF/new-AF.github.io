27e4f2bc2af6e1996b34f2ded435c0a064e31bcc
new-AF <53046436+new-AF@users.noreply.github.com>
2025-10-09 12:18:23 +0000

    Update v8 portfolio

--- assets/index-BeM_T4Gg.js
+++ assets/index-BeM_T4Gg.js
@@ -55,7 +55,7 @@ Boom.
 
 # Developer Experience (DX)
 
-I wrote 2 versions of Bubble Sort using both indexing schemes, and _objectively_ it is clear which is **less prone to off-by-one errors** but is also easier to understand.
+I wrote 2 versions of Bubble Sort using both indexing schemes, and it is _objectively_ clear which is _**less prone to off-by-one errors**_ but is also _easier to understand_.
 
 ## 1-Based Bubble Sort
 
