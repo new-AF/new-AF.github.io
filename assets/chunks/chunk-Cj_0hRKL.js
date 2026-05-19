a95ef0edb7b78ecda46366da2322f70901e7e422
new-AF <53046436+new-AF@users.noreply.github.com>
2026-05-19 15:50:49 +0000

    Update v8 portfolio

--- assets/chunks/chunk-Cj_0hRKL.js
+++ assets/chunks/chunk-Cj_0hRKL.js
@@ -1,4 +1,4 @@
-import{j as m,m as g}from"./chunk-mbTCly4g.js";/**
+import{j as m,m as g}from"./chunk-DUv04mq6.js";/**
  * mergeClassNames - A straightforward utility for merging CSS class names in React + Tailwind, and other JavaScript projects.
  *
  * @license AGPL-3.0
